15de2450adcac467604a1f6960abc9a64522581f
Yaolong Huang <airekans@gmail.com>
2020-04-19 18:40:31 -0700

    Update temp_alias.cpp

--- temp_alias.cpp
+++ temp_alias.cpp
@@ -7,9 +7,13 @@ using namespace std;
 template<typename T>
 using StringMap = map<string, T>;
 
+template<typename T>
+using IntMap = map<int, T>;
+
 int main(int argc, char** argv)
 {
 	StringMap<int> m;
-
+	IntMap<double> m2;
+	
 	return 0;
 }
